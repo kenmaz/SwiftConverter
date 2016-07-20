e119bb46ed81dbd7e48cf4fff68b9c042f232e1c
Kentaro Matsumae <kentaro.matsumae@dena.com>
2016-07-20 16:00:47 +0900

    Initial Commit

---
+++ SwiftConverter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftConverter
+//
+//  Created by Kentaro Matsumae on 2016/07/20.
+//  Copyright © 2016年 kenmaz.net. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftConverter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftConverter
+//
+//  Created by Kentaro Matsumae on 2016/07/20.
+//  Copyright © 2016年 kenmaz.net. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
